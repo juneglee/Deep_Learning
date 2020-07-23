7ac2799fce81e375d8ec60f660094af6ac2b6bac
juneglee <klcpop1@gmail.com>
2020-07-23 23:02:52 +0900

    [DL] chapter04 자연어 처리(NLP)

--- Deep-Learning/deepForAll/chapter04/ex00.py
+++ Deep-Learning/deepForAll/chapter04/ex00.py
@@ -1,2 +1,17 @@
 # 자연어 처리를 위한 NLTK와 KoNLPy 설치하기
 
+# NLTK 설치 
+# : 엔엘티케이(NLTK)는 자연어 처리를 위한 파이썬 패키지
+# pip install nltk
+
+# ipython
+# import nltk
+# nltk.__version__
+# nltk.download()
+
+# KoNLPy 설치 
+# : 코엔엘파이(KoNLPy)는 한국어 자연어 처리를 위한 형태소 분석기 패키지
+# pip install konlpy
+# 이후 
+# JDK  1.7 버전 이상으로 설치해야 가능하며
+# eclipse 설정에서 했던것과 마찬가지로 환경변수에서 JAVA_HOME을 이용하여 설치 경로를 설정해야 한다 
